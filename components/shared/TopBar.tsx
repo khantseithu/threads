3ed9f6bd9b534e332b8a15a69d28059e23b3e1b8
Khant Sithu <khantseithuu@gmail.com>
2023-08-05 05:07:12 +0000

    feat: side bar

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignIn, SignOutButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,6 +9,26 @@ export default function TopBar() {
         <Image src={'/assets/logo.svg'} alt="logo" width={38} height={38} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <Image src={'/assets/logout.svg'} alt="signout" width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            }
+          }}
+        />
+      </div>
     </nav>
   );
 }
